cba4c25a4536d66266c35ece45ebfc14b776463e
Marta Buda <bmarta1991@o2.pl>
2014-10-07 18:22:26 +0200

    add book new page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,20 @@
 class BooksController < ApplicationController
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path, notice: 'Saved book'
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
 end
