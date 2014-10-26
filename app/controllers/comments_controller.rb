1301bc1bd94dfbee6d701c5ef879dd9fe5ec4380
Marta Buda <bmarta1991@o2.pl>
2014-10-26 16:29:41 +0100

    add user_id to comments #create, not logged user can not write comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,15 @@ class CommentsController < ApplicationController
     @book = Book.find(params[:comment][:book_id])
     @review = Review.where('book_id = :book_id AND id = :review_id', params[:comment]).all.first
     @comment = @review.comments.new(comment_params)
-    if @comment.save
-      redirect_to book_review_path(@book, @review)
+    if current_user
+      @comment.user_id = current_user.id
+      if @comment.save
+        redirect_to book_review_path(@book, @review),
+          notice: 'Your comment is saved'
+      end
+    else
+      redirect_to login_path,
+        notice: 'You must be logged in to write comments'
     end
   end
 
