2f6a4bf33fd11e1f8789d5c2ead441342e26015c
Marta Buda <bmarta1991@o2.pl>
2014-10-29 18:19:13 +0100

    add #show spec to reviews_controller_spec

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 describe ReviewsController do
-  let!(:resource) { create(:review) }
+  let!(:book) { create(:book) }
+  let!(:resource) { create(:review, book: book) }
 
-  it_behaves_like "GET #index" do
+  it_behaves_like 'GET #index' do
     subject { :reviews }
+  end
+
+  describe 'GET #show' do
+    it 'renders the template' do
+      get :show, { book_id: book, id: resource }
+      expect(response).to render_template :show
+    end
   end
 end
 
