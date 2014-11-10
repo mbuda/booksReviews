7d09c6938f7e6b4cd5fbd9c3a20ca0f636c62b56
Marta Buda <bmarta1991@o2.pl>
2014-11-10 18:14:02 +0100

    add review#show spec

---
+++ spec/views/reviews/show_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+describe 'reviews/show' do
+  subject { page }
+  let!(:book) { create(:book) }
+  let!(:user) { create(:user) }
+  let(:review) { create(:review, book: book, user: user) }
+  before do
+    visit book_review_path(book, review)
+  end
+
+  it 'contains headline' do
+    expect(page).to have_selector(
+      'h1', text: 'Review info')
+  end
+
+  it 'have header for date added' do
+    expect(page).to have_selector(
+      'dt', text: 'Date added:')
+  end
+
+  it 'have header for review title' do
+    expect(page).to have_selector(
+      'dt', text: 'Title:')
+  end
+
+  it 'displays review title' do
+    expect(page).to have_selector(
+      'dd', text: 'Example')
+  end
+
+  it 'have header for reviewer' do
+    expect(page).to have_selector(
+      'dt', text: 'Reviewer:')
+  end
+
+  it 'displays reviewer nick' do
+    expect(page).to have_selector(
+      'dd', text: 'Johny')
+  end
+
+  it 'have header for review text' do
+    expect(page).to have_selector(
+      'dt', text: 'Text:')
+  end
+
+  it 'displays review text' do
+    expect(page).to have_selector(
+      'dd', text: 'Everything is awesome')
+  end
+end
