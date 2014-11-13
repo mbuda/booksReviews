70f65580e0c8b5e4e2d0038cf22957ab26eb6c96
Marta Buda <bmarta1991@o2.pl>
2014-11-13 18:51:42 +0100

    add books#edit spec

---
+++ spec/views/books/edit_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe 'books/edit' do
+  let(:book) { create(:book) }
+  subject { page }
+
+  before do
+    sign_in
+    visit edit_book_path(book)
+  end
+
+  it 'displays headline' do
+    expect(page).to have_selector(
+      'h1', text: 'Edit book')
+  end
+
+  it 'render form partial' do
+    response.should render_template(:partial => "_form")
+  end
+
+  describe 'form' do
+    it 'have title label' do
+      expect(page).to have_selector(
+        'label', text: 'Title')
+    end
+
+    it 'have title field' do
+      expect(page).to have_field('Title')
+    end
+
+    it 'have author label' do
+      expect(page).to have_selector(
+        'label', text: 'Author')
+    end
+
+    it 'have author field' do
+      expect(page).to have_field('Author')
+    end
+  end
+end
